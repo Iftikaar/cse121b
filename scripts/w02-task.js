b119100bf68d39c74f2f1258faa7fb4b2be3032b
Iftikaar <iftikaar0905@gmail.com>
2024-01-28 11:15:20 +0200

    correction

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,52 +3,30 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-let fullName = `Iftikaar Gaffoor`;
-
-let currentYear = `2024`;
-
-let profilePicture = `images/24991ebb-3051-4dcd-bc59-3663f2dc6bcb`;
-
-
-
+const fullName = "Iftikaar Gaffoor";
+let currentYear = new Date().getFullYear();
+let profilePicture = "images/24991ebb-3051-4dcd-bc59-3663f2dc6bcb.jpeg";
 
 /* Step 3 - Element Variables */
-
-
 const nameElement = document.getElementById("name");
-
-const foodElement = document.getElementById("food");
-
-const yearElement = document.querySelector("#year");
-
-const imagesElement = document.getElementById("images");
-
+const foodELement = document.getElementById("food");
+let yearElement = document.querySelector("#year");
+const imageElement = document.getElementsByTagName("img")[0];
 
 /* Step 4 - Adding Content */
-
-
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
-
-yearElement.textContent = `<strong>${currentYear}</strong>`;
-
-imagesElement.setAttribute(`24991ebb-3051-4dcd-bc59-3663f2dc6bcb`, profilePicture);
-
-imagesElement.setAttribute(`Profile picture of ${fullName}`, fullName )
+yearElement.textContent = currentYear;
+imageElement.setAttribute("src", profilePicture);
+imageElement.setAttribute("alt", `Profile image of ${fullName}`);
 
 /* Step 5 - Array */
+let favoriteFood = ["Pap","Dhal gos", "Beans", "Chicken", "Pasta", "Braai", "Fish"];
 
-
-
-const favFoods = ["pasta", "mutton curry", "chicken alfredo", "burgers"];
-
-console.log(food);
-
-foods.push("fried chicken", "grilled cheese", "braai meat");
-
-foodElement.innerHTML += `<br>${favFoods}`;
-
-favFoods.pop(0);
-
-foodElement.innerHTML += `<br>${favFoods}`;
-
-console.log(food);
+foodELement.innerHTML += `<br>${favoriteFood}`;
+let missingFood = "Burger";
+favoriteFood.push(missingFood);
+foodELement.innerHTML += `<br>${favoriteFood}`;
+favoriteFood.shift();
+foodELement.innerHTML += `<br>${favoriteFood}`;
+favoriteFood.pop();
+foodELement.innerHTML += `<br>${favoriteFood}`;
